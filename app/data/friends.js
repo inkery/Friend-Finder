df1da53f3337d55482592a8105ff2ca252360faf
inkery <kotalicious@outlook.com>
2019-03-01 11:20:13 -0500

    yay

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [{
+        "name": "Ashley Samson",
+        "photo": "https://i.ytimg.com/vi/h9ifhY0Sj_A/maxresdefault.jpg",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+        "name": "Happy Harry",
+        "photo": "https://i.dailymail.co.uk/i/pix/2017/07/18/14/427394C200000578-0-image-a-23_1500384481541.jpg",
+        "scores": [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+        "name": "Jonah Hill",
+        "photo": "http://www4.pictures.zimbio.com/gi/Funny+People+Q+A+Session+QPtau9yuhfPl.jpg",
+        "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+        "name": "Sammy Saxson",
+        "photo": "http://morris-photographics.com/photography/photos/people/Children15.jpg",
+        "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+        "name": "Jennifer Oxston",
+        "photo": "https://orexca.com/images/fotogallery/img_full/karshi_people_7-7.jpg",
+        "scores": [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+        "name": "Derrik Maxwell",
+        "photo": "https://i.ytimg.com/vi/H9-Qoi1lcN4/maxresdefault.jpg",
+        "scores": [3, 3, 4, 1, 4, 3, 5, 3, 5, 4]
+    },
+    {
+        "name": "Jeff Stinson",
+        "photo": "https://images.vice.com/vice/images/galleries/meta/2016/09/27/piobbico-ugly-people-world-association-festival-876-457-1474976159.jpg",
+        "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    }
+
+]
+
+module.exports = friends;
---
+++ app/routing/api-routes.js
@@ -0,0 +1,84 @@
+
+var friends = require('../data/friends.js');
+
+
+module.exports = function(app) {
+
+    
+    app.get('/api/friends', function(req, res) {
+        res.json(friends);
+    });
+
+    // API POST Requests
+    app.post('/api/friends', function(req, res) {
+
+        //Comparing user with their best friend match 
+        var totalDifference = 0;
+        //Object to hold the best match
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+
+        // Here we take the result of the user's survey POST and parse it.
+        var userData = req.body;
+        var userName = userData.name;
+        var userScores = userData.scores;
+        // Converting the users score to a number (Instead of string)
+        var b = userScores.map(function(item) {
+            return parseInt(item, 10);
+        });
+        userData = {
+            "name": req.body.name,
+            "photo": req.body.photo,
+            "scores": b
+        }
+
+
+        console.log("Name: " + userName);
+        console.log("User Score " + userScores);
+        // Converting the users score to a sum number (Adds up all the numbers in array)
+        var sum = b.reduce((a, b) => a + b, 0);
+        console.log("Sum of users score " + sum);
+        console.log("Best match friend diff " + bestMatch.friendDifference);
+
+
+        console.log("+++++++=================++++++++++");
+        // Loop through all the friend possibilities in the database. 
+        for (var i = 0; i < friends.length; i++) {
+
+            console.log(friends[i].name);
+            totalDifference = 0;
+            console.log("Total Diff " + totalDifference);
+            console.log("Best match friend diff " + bestMatch.friendDifference);
+
+            var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
+            console.log("Total friend score " + bfriendScore);
+            totalDifference += Math.abs(sum - bfriendScore);
+            console.log(" -------------------> " + totalDifference);
+           
+    
+            if (totalDifference <= bestMatch.friendDifference) {
+
+                // Reset the bestMatch to be the new friend. 
+                bestMatch.name = friends[i].name;
+                bestMatch.photo = friends[i].photo;
+                bestMatch.friendDifference = totalDifference;
+                // }
+
+            }
+            console.log(totalDifference + " Total Difference");
+
+        }
+        console.log(bestMatch);
+
+        friends.push(userData);
+        console.log("New User added");
+        console.log(userData);
+        // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page. 
+        res.json(bestMatch);
+
+    });
+
+}
